d687fbf18e270a0093fcbcf8b41386e0618db588
BIGBENYE <55766213+BIGBENYE@users.noreply.github.com>
2020-02-26 12:20:40 -0800

    Add files via upload

---
+++ Basicpython.py
@@ -0,0 +1,22 @@
+print("Hello,World!")
+print("Welcome to my first program")
+
+#George is a string because there are quotes around it
+name = "George"
+print(name)
+
+print("Welcome," + name + ", to my program!")
+
+userName = input("What's your name?")
+print(userName + "What a cool name!")
+
+#10 is an integer
+age = 10
+
+#3.25 is a float beacuse it is number with decimals
+gpa = 3.25
+
+print(name + gpa)
+
+favNum = input("What's your favorite number? ")
+print(age + favNum)
